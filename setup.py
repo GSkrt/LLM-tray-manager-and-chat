22395db6f4ccfcff4c99fecf32de45294a38db9c
Gregor Skrt <gregor.skrt@gmail.com>
2026-01-01 16:30:46 +0100

    Doing some cleanup before release

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup
 
-setup(
-    name="llm-tray-manager",
-    version="1.0",
-    description="LLM Tray Manager - Manage LLM models from the system tray",
-    long_description="A system tray application to manage LLM models , providing an simple LLM chat interface.",
-    author="Gregor Skrt",
-    author_email="gregor.skrt@gmail.com",
-    py_modules=["llm_tray_manager"],
-    install_requires=[
-        "PyQt5",
-        "docker",
-    ],
-    entry_points={
-        "console_scripts": [
-            "llm-tray-manager=llm_tray_manager:main",
+if __name__ == "__main__":
+    setup(
+        name="llm-tray-manager",
+        version="1.0",
+        description="LLM Tray Manager - Manage LLM models from the system tray",
+        long_description="A system tray application to manage LLM models , providing an simple LLM chat interface.",
+        author="Gregor Skrt",
+        author_email="gregor.skrt@gmail.com",
+        py_modules=["llm_tray_manager"],
+        install_requires=[
+            "PyQt5",
+            "docker",
         ],
-    },
-    data_files=[
-        ("share/llm-tray-manager/images", ["images/llm_tray_default.png", "images/llm_tray_gpu.png", "images/llm_tray_cpu_running.png", "images/llm_tray_gpu_running.png", "images/llm_tray_not_running.png", "images/llm_tray_error.png"]),
-        ("share/applications", ["llm-tray-manager.desktop"]), # Assuming this file will be renamed
-        ("share/doc/llm-tray-manager", ["LICENSE.txt"]),
-    ],
-)
+        setup_requires=["setuptools"],
+        entry_points={
+            "console_scripts": [
+                "llm-tray-manager=llm_tray_manager:main",
+            ],
+        },
+        data_files=[
+            ("share/llm-tray-manager/images", ["images/llm_tray_default.png", "images/llm_tray_cpu_running.png", "images/llm_tray_gpu_running.png", "images/llm_tray_not_running.png"]),
+            ("share/applications", ["llm-tray-manager.desktop"]), # Assuming this file will be renamed
+            ("share/doc/llm-tray-manager", ["LICENSE.txt"]),
+        ],
+        zip_safe=False,
+    )
